decd6dd96187d87a385e68949ee212964b6b4f5d
水华 <mizuka.wu@outlook.com>
2022-02-08 22:58:22 +0800

    feat: a

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
     <el-table :data="data">
-      <el-table-column label="a" prop="a"></el-table-column>
+      <el-table-column fixed="left" label="a" prop="a"></el-table-column>
       <el-table-column label="b" prop="b"></el-table-column>
       <el-table-column label="c" prop="c"></el-table-column>
-      <el-table-column label="d" prop="d"></el-table-column>
+      <el-table-column label="d" width="9000" prop="d"></el-table-column>
     </el-table>
+    <el-scrollbar style="white-space: nowrap;">
+      <el-tag v-for="(a, index) of data" :key="index">{{ index }}</el-tag>
+    </el-scrollbar>
   </div>
 </template>
 
